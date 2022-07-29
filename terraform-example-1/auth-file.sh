0465dc63a090b8e93e681691164f17fc246e5810
Samaneh Sadegh <samaneh.sadegh@gwdg.de>
2022-07-29 05:38:52 -0400

    Create auth-file.sh

---
+++ terraform-example-1/auth-file.sh
@@ -0,0 +1,19 @@
+export RANCHER_TOKEN_KEY=$(cat ./rancher-token.txt )
+  
+export CLUSTER_ID='c-xxx'
+export API_URL='https://rancher.mydomain.com/v3/clusters/'$CLUSTER_ID'?action=generateKubeconfig'
+export KUBE_CONFIG_PATH="~/.kube/config"
+
+curl -u "${RANCHER_TOKEN_KEY}" \
+-X POST \
+-H 'Accept: application/json' \
+-H 'Content-Type: application/json' \
+"${API_URL}" | jq -r '.config' > config
+
+cp config ~/.kube/
+rm config
+
+#check cluster
+echo "****** CLUSTER INFORMATION  ******"
+kubectl cluster-info
+kubectl get node
