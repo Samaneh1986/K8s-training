c1f3fb33aa70f394fbb71e8d11451fce5f32192b
Samaneh Sadegh <samaneh.sadegh@gwdg.de>
2022-08-11 03:58:08 -0400

    Update install_terraform.sh

--- terraform-example-1/install_terraform.sh
+++ terraform-example-1/install_terraform.sh
@@ -1,5 +1,5 @@
-wget -qO - terraform.gpg https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/terraform-archive-keyring.gpg
-sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/terraform-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/terraform.list
+curl -s https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo echo "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/terraform.list
 sudo apt update
 sudo apt install terraform
 terraform -version
